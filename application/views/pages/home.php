f3e443f285e662d0207fdb18e393d0d07c829f4a
mrgriever93 <alexandre.santos1@hotmail.com>
2014-03-18 16:47:02 +0000

    viewport

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,6 +3,7 @@
     <?php ?>
     <head>
         <title>GRUPO SOSOARES</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/sosoares/css/reset.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/sosoares/css/bootstrap.css">
