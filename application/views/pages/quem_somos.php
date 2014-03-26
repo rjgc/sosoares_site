9ecde94fd30285433e954a88a8d88524a2ccd3cf
mrgriever93 <alexandre.santos1@hotmail.com>
2014-03-26 15:17:14 +0000

    quem somos

---
+++ application/views/pages/quem_somos.php
@@ -0,0 +1,21 @@
+<div class="container candidaturas">
+    <div class="row">
+        <div class="col-md-12">
+            <ul class="breadcrumb">
+                <li><a href="<?=site_url('pages/home_caixilharia')?>">Início</a></li>
+                <li><a href="#">Grupo Sosoares</a></li>
+                <li>Quem somos</li>
+            </ul>
+            <h1 class="title3">Quem somos</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8">
+            <p>Uma parceria contínua....</p>
+            <p>O Grupo Sosoares tem vindo a despender de cada vez mais recursos e sinergias na área do desenvolvimento de novas gamas de produtos para a arquitectura.</p>
+            <p>O Gabinete de Desenvolvimento do Grupo Sosoares está cada vez mais vocacionado para responder às necessidades dos nossos clientes, mas também ir ao encontro das necessidades específicas do projectista.</p>
+            <p>Esta “parceria” permite responder às grandes exigências e necessidades do mercado, tendo sempre como base os conceitos fundamentais da Inovação, Arquitectura e Design.</p>
+            <img src="<?= base_url() ?>assets/sosoares/img/quem_somos_img.jpg">
+        </div>
+    </div>
+</div>
