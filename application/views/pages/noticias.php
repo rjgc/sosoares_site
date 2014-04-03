ba9a28e8b9c4f35f8a56bc60ee107255304caba2
mrgriever93 <alexandre.santos1@hotmail.com>
2014-04-03 14:23:14 +0100

    noticias 0.1

---
+++ application/views/pages/noticias.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <img src="gasdasdas.jpg" width="300" height="180" alt="Imagem teste" title="Imagem teste">
+        </div>
+        <div class="col-md-9">
+            <h3>Titulo X</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras egestas bibendum orci, sed interdum augue bibendum vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed at mauris sem. Nunc vel odio a nisl posuere consequat. Aliquam aliquam feugiat purus eu accumsan. Donec tortor dolor, tempor a eros ac, vulputate convallis purus. In porttitor, lacus ut bibendum lobortis, erat sem cursus libero, nec viverra est diam at dolor. Sed sed sapien condimentum, ullamcorper nibh tincidunt, consectetur orci. Fusce non euismod odio. Sed ornare adipiscing mauris sed facilisis. Curabitur sit amet quam sit amet neque dictum aliquet eget quis dui. Suspendisse nisi dui, bibendum non congue vel, ultricies a leo. Proin erat turpis, vehicula sit amet velit a, tempus rutrum nulla. Nulla suscipit erat scelerisque sem consectetur, quis tempus ante tristique.</p>
+            <p>Fusce interdum viverra urna, et dignissim leo gravida dapibus. Nam dapibus sodales porta. Aliquam suscipit vestibulum leo interdum ultricies. Curabitur vel elit pulvinar, sagittis ante in, interdum ante. Etiam volutpat orci in orci dignissim, vitae vehicula quam tincidunt. Vivamus malesuada, sapien at viverra mattis, risus odio mollis ante, in porta odio dolor a lectus. Donec feugiat massa eu eros molestie cursus id eget nibh. Donec a gravida justo, nec convallis nunc. Etiam egestas scelerisque arcu, non eleifend mi rutrum vel.</p>
+            <p>Suspendisse ullamcorper facilisis lacinia. Suspendisse potenti. Phasellus accumsan diam nec justo vestibulum laoreet. Curabitur nulla turpis, tincidunt at suscipit et, sagittis in sapien. Donec accumsan gravida risus nec luctus. Mauris tristique odio congue quam aliquam, eu convallis risus elementum. Sed feugiat nisl non viverra pellentesque. Fusce vel mi porttitor, eleifend nisl ut, ullamcorper arcu. Sed a purus at massa porta imperdiet.</p>
+            <p>Proin suscipit convallis dolor dictum vestibulum. Integer aliquam neque ac felis molestie lacinia. In rhoncus tortor gravida turpis consequat congue eu eu eros. Suspendisse vitae iaculis libero. Nullam id faucibus mauris. Cras eu sem non odio rutrum tristique. Vivamus tellus lorem, consectetur at hendrerit a, pharetra nec sapien. Sed lacinia nisi eu risus posuere lacinia.</p>
+        </div>
+    </div>
+</div>
