66000a3d1caae9680cc8d1197bcf577ab6758148
AaraoT <aarao_teixeira@hormail.com>
2014-08-07 17:24:31 +0100

    areas comerciais 0.1

--- assets/sosoares/js/areas_comerciais.js
+++ assets/sosoares/js/areas_comerciais.js
@@ -8,6 +8,12 @@ $( document ).ready(function() {
 				$('.col-md-3 p#nome').text(area[i].nome);
 				$('.col-md-3 p#telefone').text(area[i].telefone);
 				$('.col-md-3 p#email').text(area[i].email);
+
+				var aux = $('#image').attr('src').split('/');
+
+				var src = 'areas_comerciais_'+ $(this).attr('alt') + '.png';
+
+				$('#image').attr('src', $('#image').attr('src').replace(aux[aux.length-1], src));
 			}
 		};
 	},function() {
@@ -16,6 +22,12 @@ $( document ).ready(function() {
 		$('.col-md-3 p#nome').text('');
 		$('.col-md-3 p#telefone').text('');
 		$('.col-md-3 p#email').text('');
+
+		var aux = $('#image').attr('src').split('/');
+
+		var src = 'areas_comerciais.png';
+
+		$('#image').attr('src', $('#image').attr('src').replace(aux[aux.length-1], src));
 	});
 
 	$("map area").on("tap",function(){
@@ -27,7 +39,25 @@ $( document ).ready(function() {
 				$('.col-md-3 p#nome').text(area[i].nome);
 				$('.col-md-3 p#telefone').text(area[i].telefone);
 				$('.col-md-3 p#email').text(area[i].email);
+
+				var aux = $('#image').attr('src').split('/');
+
+				var src = 'areas_comerciais_'+ $(this).attr('alt') + '.png';
+
+				$('#image').attr('src', $('#image').attr('src').replace(aux[aux.length-1], src));
 			}
 		};
+	},function() {
+		$('.col-md-3 h1').text('');
+		$('.col-md-3 p#morada').text('');
+		$('.col-md-3 p#nome').text('');
+		$('.col-md-3 p#telefone').text('');
+		$('.col-md-3 p#email').text('');
+
+		var aux = $('#image').attr('src').split('/');
+
+		var src = 'areas_comerciais.png';
+
+		$('#image').attr('src', $('#image').attr('src').replace(aux[aux.length-1], src));
 	});
 });
